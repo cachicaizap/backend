b3f5378d048d38898441f60267415ed2b132de95
cachicaizap <68747922+cachicaizap@users.noreply.github.com>
2022-01-07 15:47:56 -0500

    Version 5.1

--- src/graphql/resolvers/person.ts
+++ src/graphql/resolvers/person.ts
@@ -7,8 +7,6 @@ export const personResolver: IResolvers = {
     Query: {
         getPerson(root: void, args: any){
             const [found] = data.people.filter(p => p._id === args._id)
-
-            console.log(found)
             return found
         }
     },
@@ -17,13 +15,23 @@ export const personResolver: IResolvers = {
             return obj.age ? 'Male' : 'Female'
         }
     },
-    Male:{
+    Male: {
         countries(parent: any){
             const countries : any = []
-            parent.countries.array.forEach((countryId: any) => {
-                countries.push(...data.countries.filter(c => c.id === countryId))
-            });
+            parent.countries.forEach((countryId: any) =>
+                countries.push(...data.countries.filter(c => c._id === countryId))
+            )
+            data.countries.filter(c => console.log(c.name))
             return countries
+        }
+    },
+    Country:{
+        people(parent: any){
+            const chars : any = []
+            parent.people.array.forEach((charId: any) => {
+                chars.push(... data.people.filter(c => c._id === charId))
+            });
+            return chars
         }
     }
 }
